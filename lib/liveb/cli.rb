a7c629c272f7bb84c53c39d86efe19428e8c0b50
DilumN <dilumnavanjana@gmail.com>
2016-11-09 22:45:04 +0530

    send & db configuration methods initialized

--- lib/liveb/cli.rb
+++ lib/liveb/cli.rb
@@ -3,9 +3,14 @@ require 'thor'
 module Liveb
   class CLI < Thor
 
-    desc 'accounts', 'List accounts'
-    def accounts
-      puts "accounts hits"
+    desc 'send', 'send post to live blog'
+    def send
+      puts "send post hits"
+    end
+
+    desc 'db', 'configure db connection'
+    def db
+      puts "db hits"
     end
 
   end
